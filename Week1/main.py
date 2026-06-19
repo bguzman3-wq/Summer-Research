f78637d44e561424a19f9e881282e5a40cbec69c
bguzman3-wq <bguzman3@uco.edu>
2026-06-19 12:12:57 -0500

    trying to display kill count in a pie chart

--- Week1/main.py
+++ Week1/main.py
@@ -14,11 +14,13 @@ print(df.isnull().sum())
 
 print(df['sentiment'].value_counts())
 
-kill_count = df['text'].str.contains('kill', case=False, na=False).sum()
+kill_count = df['text'].str.contains('kill', case=False, na=False).sum() ##"kill" count
 print(f"Number of tweets containing 'kill': {kill_count}")
 print(f"out of: {len(df)} tweets")#total num of tweets
 
 
 df['sentiment'].value_counts().plot(kind='bar')
+##df['text'].value_counts().plot(kind='pie') trying to display kill count in a pie chart
 sns.countplot(data=df, x='sentiment')
 plt.show()
+##plt.show()
